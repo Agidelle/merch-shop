25a8ab407fbd8e3c5e7fa9633524cdcf06c762fb
Agidelle <agidelleds@gmail.com>
2025-08-22 16:29:54 +0300

    init

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,7 +1,45 @@
 package storage
 
-import "database/sql"
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 type Storage struct {
-	db *sql.DB
+	pool *pgxpool.Pool
+}
+
+func NewPool(ctx context.Context, dsn string) *Storage {
+	cfg, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		// If the DSN is invalid, log the error and exit
+		log.Fatalf("Unable to parse database URL: %v\n", err)
+	}
+
+	cfg.MaxConns = 10 // Set a maximum number of connections
+	cfg.MinConns = 1  // Set a minimum number of connections
+	cfg.HealthCheckPeriod = 30 * time.Second
+
+	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		log.Fatalf("Unable to create connection pool: %v\n", err)
+	}
+
+	// Check if the connection pool is working
+	err = pool.Ping(ctx)
+	if err != nil {
+		pool.Close()
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+
+	return &Storage{pool: pool}
+}
+
+func (s *Storage) Close() {
+	if s.pool != nil {
+		s.pool.Close()
+	}
 }
